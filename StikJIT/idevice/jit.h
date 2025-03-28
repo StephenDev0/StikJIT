2295015a2be5289070bfa875084739f68818bc61
Dawn <cool5tarXV@gmail.com>
2025-03-28 10:12:30 +0800

    Update jit.h

--- StikJIT/idevice/jit.h
+++ StikJIT/idevice/jit.h
@@ -9,6 +9,8 @@
 #ifndef JIT_H
 #define JIT_H
 
+int mount_lower_than_17(char* imagepath,char* signature_path);
+int mount_personalized(char* imagetype,char* trustcache_path, char* manifest_path);
 int debug_app(const char *bundle_id);
 
 #endif /* JIT_H */
